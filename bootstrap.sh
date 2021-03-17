ce85a86c7015466676b360add1a976aefb7fce07
Jakob Sigurðsson <jakob.sigurdsson@netapp.com>
2021-03-17 11:24:19 +0000

    minor changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ mkdir /home/vagrant/Downloads
 cd /home/vagrant/Downloads
 
 # download prometheus installation files
-wget https://github.com/prometheus/prometheus/releases/download/v2.21.0/prometheus-2.21.0.linux-amd64.tar.gz
+wget --quiet https://github.com/prometheus/prometheus/releases/download/v2.25.2/prometheus-2.25.2.linux-amd64.tar.gz
 
 # create directory for prometheus installation files
 # so that we can extrac all the files into it
@@ -15,41 +15,44 @@ mkdir -p /home/vagrant/Prometheus/server
 cd /home/vagrant/Prometheus/server
 
 # Extract files
-tar -xvzf /home/vagrant/Downloads/prometheus-2.21.0.linux-amd64.tar.gz
+tar -xvzf /home/vagrant/Downloads/prometheus-2.25.2.linux-amd64.tar.gz
 
-cd prometheus-2.21.0.linux-amd64
+cd prometheus-2.25.2.linux-amd64
 
 # check prometheus version
-./prometheus -version
+./prometheus --version
 
 # create directory for node_exporter which can be used to send ubuntu metrics to the prometheus server
 mkdir -p /home/vagrant/Prometheus/node_exporter
 cd /home/vagrant/Prometheus/node_exporter
 
 # download node_exporter
-wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz -O /home/vagrant/Downloads/node_exporter-1.0.1.linux-amd64.tar.gz
+wget --quiet https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz -O /home/vagrant/Downloads/node_exporter-1.1.2.linux-amd64.tar.gz
 
 # extract node_exporter
-tar -xvzf /home/vagrant/Downloads/node_exporter-1.0.1.linux-amd64.tar.gz
+tar -xvzf /home/vagrant/Downloads/node_exporter-1.1.2.linux-amd64.tar.gz
 
 # create a symbolic link of node_exporter
-sudo ln -s /home/vagrant/Prometheus/node_exporter/node_exporter-1.0.1.linux-amd64/node_exporter /usr/bin
+sudo ln -s /home/vagrant/Prometheus/node_exporter/node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin
 
-# edit node_exporter configuration file and add configuration so that it will automatically start in next boot
-cat <<EOF > /etc/init/node_exporter.conf
-# Run node_exporter-1.0.1.linux-amd64
+cat <<EOF > /lib/systemd/system/node_exporter.service
+[Unit]
+Description=node_exporter
 
-start on startup
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
 
-script
-   /usr/bin/node_exporter
-end script
+[Install]
+WantedBy=multi-user.target
 EOF
 
-# start service of node_exporter
-sudo service node_exporter start
+sudo cp /lib/systemd/system/node_exporter.service /etc/systemd/system/node_exporter.service
+sudo chmod 644 /etc/systemd/system/node_exporter.service
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
 
-cd /home/vagrant/Prometheus/server/prometheus-2.21.0.linux-amd64/
+cd /home/vagrant/Prometheus/server/prometheus-2.25.2.linux-amd64/
 
 # edit prometheus configuration file which will pull metrics from node_exporter
 # every 15 seconds time interval
@@ -65,7 +68,7 @@ global:
 # A scrape configuration containing exactly one endpoint to scrape:
 # Here it's Prometheus itself.
 scrape_configs:
-  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  # The job name is added as a label 'job=<job_name>' to any timeseries scraped from this config.
   - job_name: 'node-prometheus'
 
     static_configs:
@@ -76,7 +79,7 @@ EOF
 nohup ./prometheus > prometheus.log 2>&1 &
 
 # download grafana
-wget https://dl.grafana.com/oss/release/grafana_6.7.0_amd64.deb -O /home/vagrant/Downloads/grafana_6.7.0_amd64.deb
+wget --quiet https://dl.grafana.com/oss/release/grafana_6.7.0_amd64.deb -O /home/vagrant/Downloads/grafana_6.7.0_amd64.deb
 
 sudo apt-get install -y adduser libfontconfig
 
